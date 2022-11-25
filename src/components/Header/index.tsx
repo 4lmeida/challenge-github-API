0765cccef8330b2b1a52209dbca6ef89c46b2805
luis <lferreira646@gmail.com>
2022-11-25 09:47:28 -0300

    Cards responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,14 @@
-import './styles.css';
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 export default function Header() {
-
-    return(
-        <header>
-            <nav>
-                <h1>Github API</h1>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <nav>
+        <Link to="/">
+          <h1>Github API</h1>
+        </Link>
+      </nav>
+    </header>
+  );
 }
--- src/routes/Home/HomeBody/index.tsx
+++ src/routes/Home/HomeBody/index.tsx
@@ -1,17 +1,18 @@
-import Button from '../../../components/Button';
-import './styles.css';
+import { Link } from "react-router-dom";
+import Button from "../../../components/Button";
+import "./styles.css";
 
 export default function HomeBody() {
   return (
     <main>
-      <section className='pdside40'>
+      <section className="pdside40">
         <div className=" cgapi-title">
           <h2>Desafio Github API</h2>
           <h3>DevSuperior - Escola de programação</h3>
         </div>
-        <div>
-            <Button text='Começar'/>
-        </div>
+        <Link to="/search">
+          <Button text="Começar" />
+        </Link>
       </section>
     </main>
   );
