f44c74956ea63762ff8710c61ab530dbbedc5b67
luis <lferreira646@gmail.com>
2022-12-01 18:12:03 -0300

    Updates search and delete cardfind

--- src/routes/Home/Search/index.tsx
+++ src/routes/Home/Search/index.tsx
@@ -1,10 +1,58 @@
-import CardFind from "../../../components/CardFind";
+import './styles.css';
+import * as objectService from "../../../services/object-service";
+import { useState } from 'react';
+import { ObjectDTO } from '../../../models/object';
+import CardBriefing from '../../../components/CardBriefing';
 
 export default function Search() {
+
+  const [user, setUser] = useState("");
+  const [error, setError] = useState();
+
+  const [obj, setObj] = useState<ObjectDTO>();
+
+  function handleUserChange(event: any) {
+    setUser(event.target.value);
+  }
+
+  function handleBtnOnClick(event: any) {
+    event.preventDefault();
+    console.log(user)
+    objectService.findyByUser(user)
+    .then(response => {
+      setObj(response.data);
+      console.log(response.data)
+    })
+    .catch(error => {
+      setError(error.response.data)
+
+    }) 
+  }
+
   return (
     <main>
       <section className="pdside90">
-        <CardFind />
+
+      <form>
+        <div className="cgapi-container-card">
+          <div className="cgapi-content-card">
+            <div>
+              <h2>Encontre um perfil Github</h2>
+              <input
+                name="user"
+                value={user}
+                type="text"
+                placeholder="Usuário Github"
+                onChange={handleUserChange}
+              />
+              <button onClick={handleBtnOnClick} type="submit">Encontrar</button>
+            </div>
+          </div>
+        </div>
+      </form>
+
+      
+      {obj ? <CardBriefing object={obj} /> : error && <h1>Não encontrado</h1>}
       </section>
     </main>
   );
