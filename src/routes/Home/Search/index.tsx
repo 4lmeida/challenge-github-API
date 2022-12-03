fa341078c14bdf4ff4b876cfa28ffa0c4814b57a
luis <lferreira646@gmail.com>
2022-12-02 22:00:29 -0300

    updates components and route

--- src/routes/Home/Search/index.tsx
+++ src/routes/Home/Search/index.tsx
@@ -1,11 +1,10 @@
-import './styles.css';
+import "./styles.css";
 import * as objectService from "../../../services/object-service";
-import { useState } from 'react';
-import { ObjectDTO } from '../../../models/object';
-import CardBriefing from '../../../components/CardBriefing';
+import { useState } from "react";
+import { ObjectDTO } from "../../../models/object";
+import CardBriefing from "../../../components/CardBriefing";
 
 export default function Search() {
-
   const [user, setUser] = useState("");
   const [error, setError] = useState();
 
@@ -17,42 +16,47 @@ export default function Search() {
 
   function handleBtnOnClick(event: any) {
     event.preventDefault();
-    console.log(user)
-    objectService.findyByUser(user)
-    .then(response => {
-      setObj(response.data);
-      console.log(response.data)
-    })
-    .catch(error => {
-      setError(error.response.data)
-
-    }) 
+    console.log(user);
+    objectService
+      .findyByUser(user)
+      .then((response) => {
+        setObj(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setError(error.response.data);
+        setObj(undefined);
+      });
   }
 
   return (
     <main>
       <section className="pdside90">
-
-      <form>
-        <div className="cgapi-container-card">
-          <div className="cgapi-content-card">
-            <div>
-              <h2>Encontre um perfil Github</h2>
-              <input
-                name="user"
-                value={user}
-                type="text"
-                placeholder="Usuário Github"
-                onChange={handleUserChange}
-              />
-              <button onClick={handleBtnOnClick} type="submit">Encontrar</button>
+        <form>
+          <div className="cgapi-container-card">
+            <div className="cgapi-content-card">
+              <div>
+                <h2>Encontre um perfil Github</h2>
+                <input
+                  name="user"
+                  value={user}
+                  type="text"
+                  placeholder="Usuário Github"
+                  onChange={handleUserChange}
+                />
+                <button onClick={handleBtnOnClick} type="submit">
+                  Encontrar
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
 
-      
-      {obj ? <CardBriefing object={obj} /> : error && <h1>Não encontrado</h1>}
+        {obj ? ( 
+          <CardBriefing object={obj} />
+        ) : (
+          error && <h1 className="cgpi-title">Erro ao buscar usuário</h1>
+        )}
       </section>
     </main>
   );
